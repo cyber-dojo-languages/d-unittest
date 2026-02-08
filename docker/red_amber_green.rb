8cbc812ee40c7bdaed375e22d731d918d576ae8d
JonJagger <jon@kosli.com>
2026-02-08 08:49:01 +0000

    Update red/amber/green regex patterns

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,7 +1,7 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  return :red   if /core\.exception\.AssertError/.match(output)
-  return :green if /All tests passed/.match(output)
+  return :red   if /FAILED unittests/.match(output)
+  return :green if /passed unittests/.match(output)
   return :amber
 }
